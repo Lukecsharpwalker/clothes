b79632aabe33cdadce01e4a4c8d93097c66584b1
Lukasz Jablonski <lukasz.jablonski@snpgroup.com>
2019-08-12 18:01:18 +0200

    new reports

--- Ubrania_Nowy/Ubrania_ASP.NET_Nowy/Controllers/ReportsController.cs
+++ Ubrania_Nowy/Ubrania_ASP.NET_Nowy/Controllers/ReportsController.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Ubrania_ASP.NET_Nowy.Data;
 
 namespace Ubrania_ASP.NET_Nowy.Controllers
 {
     public class ReportsController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public ReportsController(
+            ApplicationDbContext context
+            )
+        {
+            _context = context;
+        }
         public ActionResult GetDailyReportData()
         {
             return View();
@@ -40,9 +49,10 @@ namespace Ubrania_ASP.NET_Nowy.Controllers
         {
             return View();
         }
-        public ActionResult ShowTodayReportData(int id)
+        public async Task<IActionResult> ShowTodayReportData()
         {
-            return View();
+            var todaySoldClothes = await _context.Clothes.Where(c => c.SoldDate.Day == DateTime.Now.Day).ToListAsync();
+            return View(todaySoldClothes);
         }
         ////////////////////////////////////
         public ActionResult GetCustomReportData()
