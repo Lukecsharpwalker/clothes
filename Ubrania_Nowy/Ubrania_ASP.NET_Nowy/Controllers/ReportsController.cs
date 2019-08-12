3272ba8e46cf54eac8d390cbe723c8c46dac577f
Lukasz Jablonski <lukasz.jablonski@snpgroup.com>
2019-08-12 17:50:18 +0200

    ReportsController planed

--- Ubrania_Nowy/Ubrania_ASP.NET_Nowy/Controllers/ReportsController.cs
+++ Ubrania_Nowy/Ubrania_ASP.NET_Nowy/Controllers/ReportsController.cs
@@ -9,85 +9,50 @@ namespace Ubrania_ASP.NET_Nowy.Controllers
 {
     public class ReportsController : Controller
     {
-        // GET: Reports
-        public ActionResult Index()
+        public ActionResult GetDailyReportData()
+        {
+            return View();
+        }
+        public ActionResult ShowDailyReportData(int id)
+        {
+            return View();
+        }
+        ////////////////////////////////////
+        public ActionResult GetMonthlyReportData()
+        {
+            return View();
+        }
+        public ActionResult ShowMonthlyReportData(int id)
+        {
+            return View();
+        }
+        ////////////////////////////////////
+        public ActionResult GetAnnualReportData()
+        {
+            return View();
+        }
+        public ActionResult ShowAnnualReportData(int id)
+        {
+            return View();
+        }
+        ////////////////////////////////////
+        public ActionResult GetTodayReportData()
+        {
+            return View();
+        }
+        public ActionResult ShowTodayReportData(int id)
+        {
+            return View();
+        }
+        ////////////////////////////////////
+        public ActionResult GetCustomReportData()
+        {
+            return View();
+        }
+        public ActionResult ShowCustomReportData(int id)
         {
             return View();
         }
 
-        // GET: Reports/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Reports/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Reports/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Reports/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Reports/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Reports/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Reports/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
